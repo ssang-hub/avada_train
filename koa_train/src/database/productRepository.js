4a9fb11d2c757dfbc37a76691cb2ebbe33853610
ssang-hub <nguyendinhsang15102001@gmail.com>
2024-03-12 17:00:16 +0700

    first commit

---
+++ koa_train/src/database/productRepository.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+const { data: products } = require("./products.json");
+
+/**
+ * @param sort
+ * @param limit
+ * @returns {[ {id: number, name: string, price: number, color: string, createdAt: string, image: string},
+ *  {id: number, name: string, price: number, color: string, createdAt: string, image: string},...]}
+ */
+function getAll(sort = "asc", limit = 1000) {
+  return products
+    .sort((a, b) => {
+      return sort === "desc"
+        ? new Date(b.createdAt) - new Date(a.createdAt)
+        : new Date(a.createdAt) - new Date(b.createdAt);
+    })
+    .slice(0, limit);
+}
+
+/**
+ * @param sort
+ * @param query_params
+ * @returns {id: number, name: string, price: number, color: string, createdAt: string, image: string}
+ */
+function getOne(id, query_params) {
+  const result = {};
+  const product = products.find((product) => product.id === parseInt(id));
+
+  if (!query_params) return product;
+
+  for (const [key, value] of Object.entries(product)) {
+    if (query_params.includes(key)) result[key] = value;
+  }
+  return result;
+}
+
+function writeJSONFile() {
+  return fs.writeFileSync(
+    "./src/database/products.json",
+    JSON.stringify({
+      data: result,
+    })
+  );
+}
+
+/**
+ *
+ * @param data
+ */
+function add(data) {
+  const date = new Date();
+  data.createdAt = `${date.getDate()}-${
+    date.getMonth() + 1
+  }-${date.getFullYear()}`;
+  const result = [data, ...products];
+  return writeJSONFile(result);
+}
+
+/**
+ *
+ * @param id
+ */
+function remove(id) {
+  const result = products.filter((item) => item.id !== parseInt(id));
+  return writeJSONFile(result);
+}
+
+/**
+ *
+ * @param data
+ * @param id
+ */
+function update(data, id) {
+  const result = products.map((item) => {
+    if (item.id === parseInt(id)) {
+      return { ...item, ...data };
+    }
+    return item;
+  });
+  return writeJSONFile(result);
+}
+
+module.exports = {
+  getOne,
+  getAll,
+  add,
+  update,
+  remove,
+};
---
+++ koa_train/src/middleware/productInputMiddleware.js
@@ -0,0 +1,44 @@
+const yup = require("yup");
+
+async function createProductMiddleware(ctx, next) {
+  try {
+    const postData = ctx.request.body;
+    let schema = yup.object().shape({
+      id: yup.number().positive().integer().required(),
+      name: yup.string().required(),
+      price: yup.number().positive().integer().required(),
+      color: yup.string().required(),
+    });
+    await schema.validate(postData);
+    next();
+  } catch (e) {
+    ctx.status = 400;
+    ctx.body = {
+      success: false,
+      errors: e.errors,
+      errorName: e.name,
+    };
+  }
+}
+
+async function updateMiddleWare(ctx, next) {
+  try {
+    const data = ctx.request.body;
+    let schema = yup.object().shape({
+      name: yup.string(),
+      price: yup.number().positive().integer(),
+      color: yup.string(),
+    });
+    await schema.validate(data);
+    next();
+  } catch (e) {
+    ctx.status = 400;
+    ctx.body = {
+      success: false,
+      errors: e.errors,
+      errorName: e.name,
+    };
+  }
+}
+
+module.exports = { createProductMiddleware, updateMiddleWare };
